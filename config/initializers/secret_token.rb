aa0e2b2b4826493d7f966ef55f10941471543158
Rohini Matale <rohineematale@gmail.com>
2013-04-18 17:43:45 +0530

    golf blog (css remain)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolfBlog::Application.config.secret_token = '468dd511bd9e2aba4481cbca88dc5d9d78512c9d41093d99ded085a37682664c72b14a59760d37c3ec231a0ef09001cc5fdfe0b208e1e25c11d5169de58603ed'
